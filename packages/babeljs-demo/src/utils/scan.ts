02bd547aa343384c03760c5245f59069d7cbff6e
wangyang.learnwy <wangyang.learnwy@bytedance.com>
2021-11-08 20:57:06 +0800

    feat: 命令可以单文件执行

--- packages/babeljs-demo/src/utils/scan.ts
+++ packages/babeljs-demo/src/utils/scan.ts
@@ -1,7 +1,7 @@
 import { parse, ParseResult } from "@babel/core";
 import chalk from "chalk";
 import ora from "ora";
-import { glob, readFileAsync } from "./fs";
+import { existsSync, glob, readFileAsync } from "./fs";
 import { join } from "./path";
 import { travelUserSelectProjects } from "./project";
 import { SimpleTask } from "./simple-task";
@@ -21,25 +21,30 @@ async function scanFile(file: string, scanFn: ScanFN): Promise<void> {
 }
 
 async function scan(taskName: string, scanFn: ScanFN) {
+	const file = process.argv[2];
 	const projectOra = ora(taskName);
 	projectOra.spinner = "dots";
-	await travelUserSelectProjects(async (name, projectPath) => {
-		projectOra.text = `scan[${chalk.green(name)}]`;
-		projectOra.start(); // 认为文件路径是 项目路径/src
+	if (file && existsSync(file)) {
+		await scanFile(file, scanFn);
+	} else {
+		await travelUserSelectProjects(async (name, projectPath) => {
+			projectOra.text = `scan[${chalk.green(name)}]`;
+			projectOra.start(); // 认为文件路径是 项目路径/src
 
-		const rootDir = join(projectPath, "src");
-		const files = await glob(rootDir + "/**/*.@(ts|js|tsx|jsx)", {});
-		const task = new SimpleTask(3);
-		task.addAll(
-			files.map((file) => () => {
-				return file.endsWith(".d.ts")
-					? Promise.resolve()
-					: scanFile(file, scanFn);
-			}),
-		);
-		await task.run();
-		projectOra.succeed(chalk.green(`scan[${name}]success`));
-	});
+			const rootDir = join(projectPath, "src");
+			const files = await glob(rootDir + "/**/*.@(ts|js|tsx|jsx)", {});
+			const task = new SimpleTask(3);
+			task.addAll(
+				files.map((file) => () => {
+					return file.endsWith(".d.ts")
+						? Promise.resolve()
+						: scanFile(file, scanFn);
+				}),
+			);
+			await task.run();
+			projectOra.succeed(chalk.green(`scan[${name}]success`));
+		});
+	}
 }
 
 export { scanFile, scan };
